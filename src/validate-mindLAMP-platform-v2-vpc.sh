e45a347f77bdca4cb0b730c194d9bbc6532c9ef2
Robert Wang <rwang5688@outlook.com>
2021-08-25 00:18:23 -0700

    add  vpc.yaml and validate-vpc.sh

---
+++ src/validate-mindLAMP-platform-v2-vpc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export MINDLAMP_PLATFORM_V2_SRC=$MINDLAMP_PLATFORM_V2_HOME/src
+
+# print environment variables
+echo "MINDLAMP_PLATFORM_V2_SRC=$MINDLAMP_PLATFORM_V2_SRC"
+
+# execute
+echo "validate-mindLAMP-platform-v2-vpc.sh"
+aws cloudformation validate-template \
+--template-body file://$MINDLAMP_PLATFORM_V2_SRC/mindLAMP-platform-v2-vpc.yaml
+
