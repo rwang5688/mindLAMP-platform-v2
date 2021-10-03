2956f482974ecfde2b1726c4407df6a787ccf8c2
Robert Wang <rwang5688@outlook.com>
2021-10-02 23:42:40 -0700

    annotate templates + example parameters json

--- config/us-east-1/update-ec2-active-passive-100.sh
+++ config/us-east-1/update-ec2-active-passive-100.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp mindLAMP-platform-v2-ec2-parameters-active-passive-100.json \
+cp mindLAMP-platform-v2-ec2-active-passive-100-parameters.json \
 mindLAMP-platform-v2-ec2-parameters.json
 
 update-mindLAMP-platform-v2-comp.sh ec2 us-east-1 default mindLAMP-platform-v2
--- config/us-east-1/update-ec2-active-passive-1000.sh
+++ config/us-east-1/update-ec2-active-passive-1000.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp mindLAMP-platform-v2-ec2-parameters-active-passive-1000.json \
+cp mindLAMP-platform-v2-ec2-active-passive-1000-parameters.json \
 mindLAMP-platform-v2-ec2-parameters.json
 
 update-mindLAMP-platform-v2-comp.sh ec2 us-east-1 default mindLAMP-platform-v2
